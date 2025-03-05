48f12d46ddad2b01e79e780e619319aacc16126c
Oliver-Bev <oliver.p.szkola@gmail.com>
2025-03-05 22:45:56 +0100

    s

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Header from "../components/header";
+import "../styles/home.css"
 const Home = () => {
-    
+    return(
+        <div className="main"></div>
+    )
 };
 
 
